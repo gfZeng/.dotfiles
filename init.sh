a145f4de1dccb21f3806bb256b20e83b8237be92
Gaofeng Zeng <ndtm.idea@gmail.com>
2014-02-09 17:01:59 +0800

    update

--- init.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-files=(.zsh_completion .zshrc .bash_profile .bashrc .emacs .emacs.d .gvimrc .tmux.conf .vim .vimrc .xinitrc)
-PWD=$(pwd)
-for f in ${files[@]}
-do
-    rm ~/$f
-    echo "~/$f ---> $PWD/$f"
-    ln -s $PWD/$f ~/$f
-done
