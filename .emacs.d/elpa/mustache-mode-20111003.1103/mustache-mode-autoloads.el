568b1dbe1b40ce35ede652ad55a0d3eaaec01151
Gaofeng Zeng <ndtm.idea@gmail.com>
2014-02-18 17:53:05 +0800

    add mustache

---
+++ .emacs.d/elpa/mustache-mode-20111003.1103/mustache-mode-autoloads.el
@@ -0,0 +1,29 @@
+;;; mustache-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (mustache-mode) "mustache-mode" "mustache-mode.el"
+;;;;;;  (21251 11308 79999 647000))
+;;; Generated autoloads from mustache-mode.el
+
+(autoload 'mustache-mode "mustache-mode" "\
+
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("mustache-mode-pkg.el") (21251 11308 226696
+;;;;;;  654000))
+
+;;;***
+
+(provide 'mustache-mode-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; mustache-mode-autoloads.el ends here
